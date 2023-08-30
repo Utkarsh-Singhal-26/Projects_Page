95df2ed97eb86fc88afb8bd3b3f9c5aa25c6ffc0
Utkarsh Singhal <118098216+Utkarsh-Singhal-26@users.noreply.github.com>
2023-08-30 14:29:04 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,74 @@
+const menu = [
+  {
+    id: 1,
+    title: "Accordion",
+    category: "Extra",
+    img: "./images/Accordion.png",
+    desc: "Utilizing the power of React, my project 'Accordion' delivers a seamless and interactive way to display and navigate through content, enhancing user experience."
+  },
+  {
+    id: 2,
+    title: "Box Shadow",
+    category: "Generator",
+    img: "./images/Box_Shadow_Generator.png",
+    desc: "My 'Box Shadow Generator' project, built with React, empowers users to effortlessly create and customize box shadows, making UI elements pop with depth and dimension."
+  },
+  {
+    id: 3,
+    title: "Multiple Column",
+    category: "Generator",
+    img: "./images/Column_Generator.png",
+    desc: "Crafted with React, my 'Multiple Column Generator' project enables users to design responsive layouts, efficiently optimizing content presentation across various screen sizes."
+  },
+  {
+    id: 4,
+    title: "Copy Code",
+    category: "Extra",
+    img: "./images/Copy_Code.png",
+    desc: "Simplifying code sharing, my React-based 'Copy Code' project streamlines the process of copying and pasting snippets, promoting seamless knowledge exchange among developers."
+  },
+  {
+    id: 5,
+    title: "Info",
+    category: "Extra",
+    img: "./images/Info.png",
+    desc: "Empowering users with information, my React-powered 'Info' project provides an intuitive and organized platform to present and absorb valuable insights."
+  },
+  {
+    id: 6,
+    title: "Reviews",
+    category: "Extra",
+    img: "./images/Reviews.png",
+    desc: "Enhancing credibility and transparency, the React-driven 'Reviews' project I've developed offers a user-friendly space for sharing and exploring authentic customer feedback."
+  },
+  {
+    id: 7,
+    title: "Template 1",
+    category: "Template",
+    img: "./images/Template-1.png",
+    desc: "Elevating visual communication, my project 'Static UI Posters' employs React to create stunning, informative posters that capture the essence of user interfaces for educational and inspirational purposes."
+  },
+  {
+    id: 8,
+    title: "Template 2",
+    category: "Template",
+    img: "./images/Template-2.png",
+    desc: "Elevating visual communication, my project 'Static UI Posters' employs React to create stunning, informative posters that capture the essence of user interfaces for educational and inspirational purposes."
+  },
+  {
+    id: 9,
+    title: "Template 3",
+    category: "Template",
+    img: "./images/Template-3.png",
+    desc: "Elevating visual communication, my project 'Static UI Posters' employs React to create stunning, informative posters that capture the essence of user interfaces for educational and inspirational purposes."
+  },
+  {
+    id: 10,
+    title: "Travel Journal",
+    category: "Extra",
+    img: "./images/Travel-Journal.png",
+    desc: "Chronicling adventures beautifully, my React-driven 'Travel Journal' project allows users to elegantly document their journeys, combining images and narratives into captivating digital chronicles."
+  },
+];
+
+export default menu;
